e5b61b388a8d9142a8ddc42f16c19f40e91131bd
John Connolly <jcconnol4@gmail.com>
2024-09-04 17:47:05 -0500

    Add basic countdown timer

---
+++ src/components/Countdown.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const CountdownContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #282c34;
+  color: white;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const TimeDisplay = styled.div`
+  font-size: 4rem;
+  margin: 20px;
+`;
+
+const Message = styled.div`
+  font-size: 2rem;
+  color: #61dafb;
+`;
+
+const Countdown = () => {
+  const [timeLeft, setTimeLeft] = useState('');
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const now = new Date();
+      const nextTarget = new Date();
+      nextTarget.setHours(13, 0, 0, 0); // 1:00 PM
+
+      if (now > nextTarget) {
+        nextTarget.setDate(nextTarget.getDate() + 1);
+      }
+
+      const timeDifference = nextTarget - now;
+
+      const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((timeDifference / 1000 / 60) % 60);
+      const seconds = Math.floor((timeDifference / 1000) % 60);
+
+      setTimeLeft(
+        `${hours.toString().padStart(2, '0')}:${minutes
+          .toString()
+          .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+      );
+    };
+
+    const intervalId = setInterval(calculateTimeLeft, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <CountdownContainer>
+      <Message>Countdown to Cafe Squad</Message>
+      <TimeDisplay>{timeLeft}</TimeDisplay>
+    </CountdownContainer>
+  );
+};
+
+export default Countdown;
